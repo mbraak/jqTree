2c27bf1e37891a9486252565c8a7104426250d1a
Marco Braak <mbraak@ridethepony.nl>
2011-05-11 19:52:44 +0200

    Use $.extend to create classes. Move Tree code to tree.jquery.js.

--- tree.jquery.js
+++ tree.jquery.js
@@ -15,8 +15,108 @@
 // todo: unit test
 // todo: documentation
 // todo: click are for toggling folder
+// todo: do not use Classy
 
 (function($) {
+    var Position = {
+        BEFORE: 1,
+        AFTER: 2,
+        INSIDE: 3
+    };
+
+    var Node = function(name) {
+        this.init(name);
+    };
+
+    $.extend(Node.prototype, {
+        init: function(name) {
+            this.name = name;
+            this.children = [];
+            this.parent = null;
+        },
+
+        loadFromData: function(data) {
+            this.children = [];
+
+            var self = this;
+            $.each(data, function() {
+                var node = new Node(this.label);
+                self.addChild(node);
+
+                if (this.children) {
+                    node.loadFromData(this.children);
+                }
+            });
+        },
+
+        addChild: function(node) {
+            this.children.push(node);
+            node.parent = this;
+        },
+
+        addChildAtPosition: function(node, index) {
+            this.children.splice(index, 0, node);
+            node.parent = this;
+        },
+
+        removeChild: function(node) {
+            this.children.splice(
+                this.getChildIndex(node),
+                1
+            );
+        },
+
+        getChildIndex: function(node) {
+            return this.children.indexOf(node);
+        },
+
+        hasChildren: function() {
+            return (this.children.length != 0);
+        },
+
+        iterate: function(callback, level) {
+            if (! level) {
+                level = 0;
+            }
+
+            $.each(this.children, function() {
+                var result = callback(this, level);
+
+                if (this.hasChildren() && result) {
+                    this.iterate(callback, level + 1);
+                }
+            });
+        },
+
+        moveNode: function(moved_node, target_node, position) {
+            // todo: check for illegal move
+            moved_node.parent.removeChild(moved_node);
+            if (position == Position.AFTER) {
+                target_node.parent.addChildAtPosition(
+                    moved_node,
+                    target_node.parent.getChildIndex(target_node) + 1
+                );
+            }
+            else if (position == Position.BEFORE) {
+                target_node.parent.addChildAtPosition(
+                    moved_node,
+                    target_node.parent.getChildIndex(target_node)
+                );
+            }
+            else if (position == Position.INSIDE) {
+                target_node.addChild(moved_node);
+            }
+        }
+    });
+
+    Node.createFromData = function(data) {
+        var tree = new Tree();
+        tree.loadFromData(data);
+        return tree;
+    };
+
+    var Tree = Node;
+
     $.widget("ui.tree", $.ui.mouse, {
         widgetEventPrefix: "tree",
         options: {
@@ -485,8 +585,12 @@
         }
     });
 
-    var NodeElement = Class.$extend({
-        __init__: function(node) {
+    var NodeElement = function(node) {
+        this.init(node);
+    };
+
+    $.extend(NodeElement.prototype, {
+        init: function(node) {
             this.node = node;
             this.$element = $(node.element)
         },
@@ -536,7 +640,11 @@
         }
     });
 
-    var FolderElement = NodeElement.$extend({
+    var FolderElement = function(node) {
+        this.init(node);
+    };
+
+    $.extend(FolderElement.prototype, NodeElement.prototype, {
         toggle: function() {
             if (this.node.is_open) {
                 this.close();
