5f90e383896640ebbdaa192a897eb8f656f745a2
Marco Braak <mbraak@ridethepony.nl>
2012-09-24 19:02:06 +0200

    Generate examples from templates

---
+++ docs_src/generate.py
@@ -0,0 +1,35 @@
+import os
+import re
+
+from jinja2 import Environment, FileSystemLoader
+
+
+env = Environment(loader=FileSystemLoader('.'))
+
+
+def generate(source_filename, target_filename):
+    print 'generate %s' % source_filename
+    template = env.get_template(source_filename)
+
+    html = template.render()
+
+    with open(target_filename, 'w') as f:
+        f.write(html)
+
+
+def get_template_files():
+    def get_filename(f):
+        return os.path.splitext(f)[0]
+
+    pattern = r'example(?P<id>\d+).tmpl'
+
+    return [
+        get_filename(f) for f in os.listdir('.')
+        if re.match(pattern, f)
+    ]
+
+
+for f in get_template_files():
+    source_filename = '%s.tmpl' % f
+    target_filename = '../examples/%s.html' % f
+    generate(source_filename, target_filename)
