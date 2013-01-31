f5c09290d51083e3662078a6027eeb9eb8228bdd
Marco Braak <mbraak@ridethepony.nl>
2013-01-31 09:30:57 +0100

    Added issue 112 to changelog

--- tree.jquery.js
+++ tree.jquery.js
@@ -239,7 +239,7 @@ limitations under the License.
   })(SimpleWidget);
 
   /*
-  Copyright 2012 Marco Braak
+  Copyright 2013 Marco Braak
   
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
