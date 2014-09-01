8c654d74b65efca72aeaae79e5f0ced06cba5de3
Marco Braak <mbraak@ridethepony.nl>
2014-09-01 07:46:44 +0200

    Use ids in examples

--- static/documentation.js
+++ static/documentation.js
@@ -27,16 +27,16 @@ $(function() {
     // demo tree
     var data = [
         {
-            label: 'node1',
+            label: 'node1', id: 1,
             children: [
-                { label: 'child1' },
-                { label: 'child2' }
+                { label: 'child1', id: 2 },
+                { label: 'child2', id: 3 }
             ]
         },
         {
-            label: 'node2',
+            label: 'node2', id: 4,
             children: [
-                { label: 'child3' }
+                { label: 'child3', id: 5 }
             ]
         }
     ];
