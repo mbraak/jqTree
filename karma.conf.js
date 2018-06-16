3ddaf6957f9381056c160d59ebd61cd6b5c533ef
Marco Braak <marco@invoicefinance.nl>
2018-06-16 16:52:00 +0200

    Use development mode for webpack

--- karma.conf.js
+++ karma.conf.js
@@ -35,6 +35,7 @@ module.exports = function(config) {
             resolve: {
                 extensions: [".ts", ".js"]
             },
+            mode: "development",
             module: {
                 rules: [
                     {
