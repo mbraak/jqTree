71e3c6c8a7fc7c2fe1245fb78901a311c63d550f
Marco Braak <marco@invoicefinance.nl>
2018-06-16 13:37:58 +0200

    Test with chrome headless

--- karma.conf.js
+++ karma.conf.js
@@ -75,7 +75,14 @@ module.exports = function(config) {
 
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        browsers: ["Chrome", "Firefox", "PhantomJS", "IE8 - WinXP"],
+        browsers: ["ChromeHeadlessNoSandbox"],
+
+        customLaunchers: {
+            ChromeHeadlessNoSandbox: {
+                base: 'ChromeHeadless',
+                flags: ['--no-sandbox']
+            }
+        },
 
         // Continuous Integration mode
         // if true, Karma captures browsers, runs the tests and exits
