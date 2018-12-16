2e78daea2b95f41e42037a905f23a04d4c71944d
Marco Braak <marco@invoicefinance.nl>
2018-12-16 15:46:16 +0100

    Update gulp and other packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,31 @@
 const gulp = require("gulp");
-const fs = require("fs");
-const rename = require("gulp-rename");
 const sass = require("gulp-sass");
 const ts = require("gulp-typescript");
 
-gulp.task("lib_ts", function () {
-    const tsProject = ts.createProject('tsconfig.json');
+gulp.task("lib_ts", function(done) {
+    const tsProject = ts.createProject("tsconfig.json");
 
     gulp.src("./src/*.ts")
         .pipe(tsProject())
         .pipe(gulp.dest("./lib"));
+
+    done();
 });
 
-gulp.task("sass", function () {
+gulp.task("sass", function(done) {
     gulp.src("./jqtree.scss")
         .pipe(sass({ errLogToConsole: true }))
-        .pipe(gulp.dest("./"))
+        .pipe(gulp.dest("./"));
+
+    done();
 });
 
-gulp.task("example_sass", function () {
+gulp.task("example_sass", function(done) {
     gulp.src("./static/example.scss")
         .pipe(sass({ errLogToConsole: true }))
         .pipe(gulp.dest("./static"));
+
+    done();
 });
 
-gulp.task("default", ["lib_ts", "sass", "example_sass"]);
+gulp.task("default", gulp.series("lib_ts", "sass", "example_sass"));
