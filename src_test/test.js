e6c8469951535f40b442b596ebed17b2f9572186
Marco Braak <marco@invoicefinance.nl>
2019-12-31 11:51:20 +0100

    Remove js files from src_test

--- src_test/test.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-require("./testJqtree");
-require("./testNodeUtil");
-require("./testTree");
-require("./testUtil");
-QUnit.config.testTimeout = 5000;
-//# sourceMappingURL=test.js.map
