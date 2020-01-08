496211d981f92f6998e55bbbb92c6f06f8e8db8b
Marco Braak <marco@invoicefinance.nl>
2020-01-08 16:17:07 +0100

    Update packages

--- build/test.js
+++ build/test.js
@@ -1,7 +1,7 @@
 /*!
  * JqTree 1.4.12
  * 
- * Copyright 2019 Marco Braak
+ * Copyright 2020 Marco Braak
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
