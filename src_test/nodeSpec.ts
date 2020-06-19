78909ff4dcfd0868ebea5d8897eed24a448c8eae
Marco Braak <marco@invoicefinance.nl>
2020-06-19 12:13:53 +0200

    Add nodeSpec

---
+++ src_test/nodeSpec.ts
@@ -0,0 +1,88 @@
+import { expect } from "chai";
+import { Node } from "../src/node";
+
+describe("constructor", () => {
+    context("with a string", () => {
+        const node = new Node("n1");
+
+        it("creates a node", () => {
+            expect(node.name).to.eq("n1");
+            expect(node.children).to.be.empty;
+            expect(node.parent).to.be.null;
+        });
+    });
+
+    context("with an object with a name property", () => {
+        const node = new Node({
+            id: 123,
+            name: "n1",
+        });
+
+        it("creates a node", () => {
+            expect(node).to.include({
+                id: 123,
+                name: "n1",
+            });
+        });
+    });
+
+    context("with an object with more properties", () => {
+        const node = new Node({
+            color: "green",
+            id: 123,
+            name: "n1",
+            url: "/abc",
+        });
+
+        it("creates a node", () => {
+            expect(node).to.include({
+                color: "green",
+                id: 123,
+                name: "n1",
+                url: "/abc",
+            });
+        });
+    });
+
+    context("with an object with a label property", () => {
+        const node = new Node({
+            id: 123,
+            label: "n1",
+            url: "/",
+        });
+
+        it("creates a node", () => {
+            expect(node).to.include({
+                id: 123,
+                name: "n1",
+                url: "/",
+            });
+            expect(node).to.not.have.property("label");
+            expect(node.children).to.be.empty;
+            expect(node.parent).to.be.null;
+        });
+    });
+
+    context("with an object with a parent", () => {
+        const node = new Node({
+            name: "n1",
+            parent: "abc",
+        });
+
+        it("doesn't set the parent", () => {
+            expect(node.name).to.eq("n1");
+            expect(node.parent).to.be.null;
+        });
+    });
+    context("with an object with children", () => {
+        const node = new Node({
+            name: "n1",
+            children: ["c"],
+        });
+
+        it("doesn't set the children", () => {
+            expect(node.name).to.eq("n1");
+            expect(node.children).to.be.empty;
+        });
+    });
+});
--- src_test/test.ts
+++ src_test/test.ts
@@ -1,2 +1,3 @@
 import "./nodeUtilSpec";
+import "./nodeSpec";
 import "./utilSpec";
