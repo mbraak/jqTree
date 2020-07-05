9f90b54644d1a1bf6b4bbeffceb93e1cba6963b3
Marco Braak <marco@invoicefinance.nl>
2020-07-05 12:15:03 +0200

    Remove jest jquery matchers

--- src/test/support/setupTests.ts
+++ src/test/support/setupTests.ts
@@ -1,7 +1,4 @@
 import * as jQuery from "jquery";
-import * as jQueryMatchers from "jest-jquery-matchers";
 import "./jqTreeMatchers";
 
 (window as any).jQuery = jQuery; // eslint-disable-line @typescript-eslint/no-unsafe-member-access
-
-jest.addMatchers(jQueryMatchers);
