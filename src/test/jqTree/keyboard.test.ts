6c1cbe7b47545815e209f5bdc29beaa23f583641
Marco Braak <marco@invoicefinance.nl>
2020-07-24 15:29:49 +0200

    Test more keys

--- src/test/jqTree/keyboard.test.ts
+++ src/test/jqTree/keyboard.test.ts
@@ -17,11 +17,13 @@ afterEach(() => {
 
 describe("keyboard support", () => {
     interface Vars {
+        pressedKey: number;
         initialSelectedNode: INode;
         $tree: JQuery<HTMLElement>;
     }
 
     const KEY_DOWN = 40;
+    const KEY_RIGHT = 39;
     const KEY_UP = 38;
 
     const given = getGiven<Vars>();
@@ -30,16 +32,16 @@ describe("keyboard support", () => {
     beforeEach(() => {
         given.$tree.tree({ data: exampleData });
         given.$tree.tree("selectNode", given.initialSelectedNode);
+        given.$tree.trigger($.Event("keydown", { which: given.pressedKey }));
     });
 
     context("with key down", () => {
         given("initialSelectedNode", () =>
             given.$tree.tree("getNodeByNameMustExist", "node1")
         );
+        given("pressedKey", () => KEY_DOWN);
 
         test("selects the next node", () => {
-            given.$tree.trigger($.Event("keydown", { which: KEY_DOWN }));
-
             expect(given.$tree).toHaveTreeStructure([
                 expect.objectContaining({ name: "node1", selected: false }),
                 expect.objectContaining({ name: "node2", selected: true }),
@@ -51,13 +53,34 @@ describe("keyboard support", () => {
         given("initialSelectedNode", () =>
             given.$tree.tree("getNodeByNameMustExist", "node2")
         );
+        given("pressedKey", () => KEY_UP);
 
         test("selects the next node", () => {
-            given.$tree.trigger($.Event("keydown", { which: KEY_UP }));
-
             expect(given.$tree).toHaveTreeStructure([
                 expect.objectContaining({ name: "node1", selected: true }),
                 expect.objectContaining({ name: "node2", selected: false }),
+            ]);
+        });
+    });
+
+    context("with key right", () => {
+        given("initialSelectedNode", () =>
+            given.$tree.tree("getNodeByNameMustExist", "node1")
+        );
+        given("pressedKey", () => KEY_RIGHT);
+
+        test("opens the node", () => {
+            expect(given.$tree).toHaveTreeStructure([
+                expect.objectContaining({
+                    name: "node1",
+                    open: true,
+                    selected: true,
+                }),
+                expect.objectContaining({
+                    name: "node2",
+                    open: false,
+                    selected: false,
+                }),
             ]);
         });
     });
