a9a64bbd25fcc843205cac408d31a77c4ead46b9
Marco Braak <marco@invoicefinance.nl>
2020-08-03 15:06:46 +0200

    Update keyboard test

--- src/test/jqTree/keyboard.test.ts
+++ src/test/jqTree/keyboard.test.ts
@@ -17,71 +17,210 @@ afterEach(() => {
 
 describe("keyboard support", () => {
     interface Vars {
+        autoOpen: boolean;
+        initialSelectedNode: INode | null;
         pressedKey: number;
-        initialSelectedNode: INode;
         $tree: JQuery<HTMLElement>;
     }
 
     const KEY_DOWN = 40;
+    const KEY_LEFT = 37;
     const KEY_RIGHT = 39;
     const KEY_UP = 38;
 
     const given = getGiven<Vars>();
+    given("autoOpen", () => false);
+    given("initialSelectedNode", () => null);
     given("$tree", () => $("#tree1"));
 
     beforeEach(() => {
-        given.$tree.tree({ data: exampleData });
-        given.$tree.tree("selectNode", given.initialSelectedNode);
+        given.$tree.tree({
+            animationSpeed: 0,
+            autoOpen: given.autoOpen,
+            data: exampleData,
+        });
+
+        if (given.initialSelectedNode) {
+            given.$tree.tree("selectNode", given.initialSelectedNode);
+        }
+
         given.$tree.trigger($.Event("keydown", { which: given.pressedKey }));
     });
 
     context("with key down", () => {
-        given("initialSelectedNode", () =>
-            given.$tree.tree("getNodeByNameMustExist", "node1")
-        );
         given("pressedKey", () => KEY_DOWN);
 
-        it("selects the next node", () => {
-            expect(given.$tree).toHaveTreeStructure([
-                expect.objectContaining({ name: "node1", selected: false }),
-                expect.objectContaining({ name: "node2", selected: true }),
-            ]);
+        context("when a node is selected", () => {
+            given("initialSelectedNode", () =>
+                given.$tree.tree("getNodeByNameMustExist", "node1")
+            );
+
+            it("selects the next node", () => {
+                expect(given.$tree).toHaveTreeStructure([
+                    expect.objectContaining({ name: "node1", selected: false }),
+                    expect.objectContaining({ name: "node2", selected: true }),
+                ]);
+            });
+        });
+
+        context("when no node is selected", () => {
+            it("does nothing", () => {
+                expect(given.$tree.tree("getSelectedNode")).toBe(false);
+            });
         });
     });
 
     context("with key up", () => {
-        given("initialSelectedNode", () =>
-            given.$tree.tree("getNodeByNameMustExist", "node2")
-        );
         given("pressedKey", () => KEY_UP);
 
-        it("selects the next node", () => {
-            expect(given.$tree).toHaveTreeStructure([
-                expect.objectContaining({ name: "node1", selected: true }),
-                expect.objectContaining({ name: "node2", selected: false }),
-            ]);
+        context("when a node is selected", () => {
+            given("initialSelectedNode", () =>
+                given.$tree.tree("getNodeByNameMustExist", "node2")
+            );
+
+            it("selects the next node", () => {
+                expect(given.$tree).toHaveTreeStructure([
+                    expect.objectContaining({ name: "node1", selected: true }),
+                    expect.objectContaining({ name: "node2", selected: false }),
+                ]);
+            });
+        });
+
+        context("when no node is selected", () => {
+            it("does nothing", () => {
+                expect(given.$tree.tree("getSelectedNode")).toBe(false);
+            });
         });
     });
 
     context("with key right", () => {
-        given("initialSelectedNode", () =>
-            given.$tree.tree("getNodeByNameMustExist", "node1")
-        );
         given("pressedKey", () => KEY_RIGHT);
 
-        it("opens the node", () => {
-            expect(given.$tree).toHaveTreeStructure([
-                expect.objectContaining({
-                    name: "node1",
-                    open: true,
-                    selected: true,
-                }),
-                expect.objectContaining({
-                    name: "node2",
-                    open: false,
-                    selected: false,
-                }),
-            ]);
+        context("when a closed folder is selected", () => {
+            given("initialSelectedNode", () =>
+                given.$tree.tree("getNodeByNameMustExist", "node1")
+            );
+
+            it("opens the folder", () => {
+                expect(given.$tree).toHaveTreeStructure([
+                    expect.objectContaining({
+                        name: "node1",
+                        open: true,
+                        selected: true,
+                    }),
+                    expect.objectContaining({
+                        name: "node2",
+                        open: false,
+                        selected: false,
+                    }),
+                ]);
+            });
+        });
+
+        context("when an open folder is selected", () => {
+            given("autoOpen", () => true);
+            given("initialSelectedNode", () =>
+                given.$tree.tree("getNodeByNameMustExist", "node1")
+            );
+
+            it("selects the first child", () => {
+                expect(given.$tree).toHaveTreeStructure([
+                    expect.objectContaining({
+                        name: "node1",
+                        open: true,
+                        selected: false,
+                        children: [
+                            expect.objectContaining({
+                                name: "child1",
+                                selected: true,
+                            }),
+                            expect.objectContaining({
+                                name: "child2",
+                                selected: false,
+                            }),
+                        ],
+                    }),
+                    expect.objectContaining({
+                        name: "node2",
+                        selected: false,
+                    }),
+                ]);
+            });
+        });
+
+        context("when no node is selected", () => {
+            it("does nothing", () => {
+                expect(given.$tree.tree("getSelectedNode")).toBe(false);
+            });
+        });
+
+        context("when a child is selected", () => {
+            given("initialSelectedNode", () =>
+                given.$tree.tree("getNodeByNameMustExist", "child1")
+            );
+
+            it("does nothing", () => {
+                expect(given.$tree.tree("getSelectedNode")).toMatchObject({
+                    name: "child1",
+                });
+            });
+        });
+    });
+    context("with key left", () => {
+        given("pressedKey", () => KEY_LEFT);
+
+        context("when a closed folder is selected", () => {
+            given("initialSelectedNode", () =>
+                given.$tree.tree("getNodeByNameMustExist", "node3")
+            );
+
+            it("selects the previous node", () => {
+                expect(given.$tree).toHaveTreeStructure([
+                    expect.objectContaining({
+                        name: "node1",
+                        selected: false,
+                    }),
+                    expect.objectContaining({
+                        name: "node2",
+                        selected: true,
+                        children: [
+                            expect.objectContaining({
+                                name: "node3",
+                                open: false,
+                                selected: false,
+                            }),
+                        ],
+                    }),
+                ]);
+            });
+        });
+
+        context("when an open folder is selected", () => {
+            given("autoOpen", () => true);
+            given("initialSelectedNode", () =>
+                given.$tree.tree("getNodeByNameMustExist", "node2")
+            );
+
+            it("closes the folder", () => {
+                expect(given.$tree).toHaveTreeStructure([
+                    expect.objectContaining({
+                        name: "node1",
+                        open: true,
+                        selected: false,
+                    }),
+                    expect.objectContaining({
+                        name: "node2",
+                        open: false,
+                        selected: true,
+                    }),
+                ]);
+            });
+        });
+
+        context("when no node is selected", () => {
+            it("does nothing", () => {
+                expect(given.$tree.tree("getSelectedNode")).toBe(false);
+            });
         });
     });
 });
