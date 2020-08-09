b69d942df193f8873c14d9686fe00fdef8326eb7
Marco Braak <marco@invoicefinance.nl>
2020-08-09 11:45:33 +0200

    Fail cypress test

--- cypress/integration/jqtree_spec.js
+++ cypress/integration/jqtree_spec.js
@@ -6,7 +6,7 @@ describe("JQtree", () => {
     it("Displays the tree", () => {
         cy.contains("Saurischia");
         cy.contains("Ornithischians");
-        cy.contains("Herrerasaurians2").should("be.not.visible");
+        cy.contains("Herrerasaurians3").should("be.not.visible");
     });
 
     it("Selects an node", () => {
