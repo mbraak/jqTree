8b8dd30e96b142b42950fa21330e8bcf7e511045
Marco Braak <marco@invoicefinance.nl>
2020-09-27 12:53:50 +0200

    Add a debug build

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,18 @@
 import typescript from "@rollup/plugin-typescript";
 import { terser } from "rollup-plugin-terser";
 
+const debugBuild = Boolean(process.env.DEBUG_BUILD);
+
+const plugins = [typescript()];
+
+if (!debugBuild) {
+    plugins.push(terser());
+}
+
 export default {
     input: "src/tree.jquery.ts",
     output: {
-        file: "tree.jquery.js",
+        file: debugBuild ? "tree.jquery.debug.js" : "tree.jquery.js",
         format: "iife",
         globals: {
             jquery: "jQuery",
@@ -12,5 +20,5 @@ export default {
         name: "jqtree",
     },
     external: ["jquery"],
-    plugins: [typescript(), terser()],
+    plugins,
 };
