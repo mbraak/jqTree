06000b60fc797c50a898f54cbcfa1bea9a34a8de
Marco Braak <marco.braak@planet.nl>
2020-11-22 13:33:45 +0100

    Typescript 4.1 (#635)
    * Use trigger instead of click
    
    * Fix tests
    
    * Update typescript
    
    * Build js

--- src/test/jqTree/methods.test.ts
+++ src/test/jqTree/methods.test.ts
@@ -991,7 +991,7 @@ describe("reload", () => {
 
     context("with a onFinished parameter", () => {
         it("calls onFinished", () =>
-            new Promise((resolve) => {
+            new Promise<void>((resolve) => {
                 const handleFinished = () => {
                     expect(given.$tree).toHaveTreeStructure([
                         expect.objectContaining({ name: "node1" }),
@@ -1181,7 +1181,7 @@ describe("setOption", () => {
 
     it("sets an option", () => {
         given.$tree.tree("setOption", "selectable", true);
-        titleSpan(given.node1.element).click();
+        titleSpan(given.node1.element).trigger("click");
         expect(given.$tree.tree("getSelectedNode")).toMatchObject({
             name: "node1",
         });
