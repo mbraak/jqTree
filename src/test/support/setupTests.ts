355f05b9cd11a942db4be070a5bb70ff9807c326
Marco Braak <marco@invoicefinance.nl>
2021-07-03 10:29:58 +0200

    Use esModuleInterop

--- src/test/support/setupTests.ts
+++ src/test/support/setupTests.ts
@@ -1,4 +1,5 @@
-import * as jQuery from "jquery";
+import jQuery from "jquery";
 import "./jqTreeMatchers";
 
+(window as any).$ = jQuery; // eslint-disable-line @typescript-eslint/no-unsafe-member-access
 (window as any).jQuery = jQuery; // eslint-disable-line @typescript-eslint/no-unsafe-member-access
