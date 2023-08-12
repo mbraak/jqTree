8dc411a743298e071f53e9d87e47bcb8830d1934
Marco Braak <marco@floryn.com>
2023-08-12 15:13:29 +0200

    Add test

--- src/test/jqTree/options.test.ts
+++ src/test/jqTree/options.test.ts
@@ -199,18 +199,29 @@ describe("closedIcon", () => {
     });
 
     it("renders a html element", () => {
-        const closedIcon = document.createElement("span");
-        closedIcon.className = "abc";
-        closedIcon.textContent = "test";
+        const icon = document.createElement("span");
+        icon.className = "abc";
+        icon.textContent = "test";
 
         const $tree = $("#tree1");
         $tree.tree({
-            closedIcon,
+            closedIcon: icon,
             data: exampleData,
         });
 
         const $span = $tree.find("a.jqtree-toggler:first span.abc");
         expect($span.text()).toBe("test");
+    });
+
+    it("renders a default when the option is empty", () => {
+        const $tree = $("#tree1");
+        $tree.tree({
+            closedIcon: undefined,
+            data: exampleData,
+        });
+
+        const $span = $tree.find("a.jqtree-toggler:first");
+        expect($span.text()).toBe("►");
     });
 });
 
