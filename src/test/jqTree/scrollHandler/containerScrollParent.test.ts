5f2e7bdd428ea51fbd6b0ec2745f523dff7c6224
Marco Braak <marco@floryn.com>
2023-09-06 15:00:44 +0200

    Set argsIgnorePattern; add test

--- src/test/jqTree/scrollHandler/containerScrollParent.test.ts
+++ src/test/jqTree/scrollHandler/containerScrollParent.test.ts
@@ -1,16 +1,33 @@
 import ContainerScrollParent from "../../../scrollHandler/containerScrollParent";
 
-const mock$Container = ({ scrollLeft } = { scrollLeft: 0 }) => {
-    const container = {} as HTMLElement;
+interface MockJQueryElementParams {
+    height?: number;
+    innerHeight?: number;
+    offsetTop?: number;
+    scrollLeft?: number;
+}
 
-    const $container = {
-        get: (_i: number) => container,
-        innerHeight: () => 0,
-        offset: () => ({ top: 0 }),
-        scrollLeft: () => scrollLeft,
+const defaultMockJQueryElementParams: MockJQueryElementParams = {
+    height: 200,
+    innerHeight: 180,
+    offsetTop: 20,
+    scrollLeft: 0,
+};
+
+const mock$JQueryElement = (inputParams: MockJQueryElementParams) => {
+    const params = { ...defaultMockJQueryElementParams, ...inputParams };
+
+    const element = {} as HTMLElement;
+
+    const $element = {
+        get: (_) => element,
+        height: () => params.height,
+        innerHeight: () => params.innerHeight,
+        offset: () => ({ top: params.offsetTop }),
+        scrollLeft: () => params.scrollLeft,
     } as JQuery<HTMLElement>;
 
-    return $container;
+    return $element;
 };
 
 const mockContainerScrollParent = ($container: JQuery<HTMLElement>) => {
@@ -26,7 +43,7 @@ const mockContainerScrollParent = ($container: JQuery<HTMLElement>) => {
 
 describe("getScrollLeft", () => {
     it("returns the scrollLeft of the container", () => {
-        const $container = mock$Container({
+        const $container = mock$JQueryElement({
             scrollLeft: 10,
         });
         const containerScrollParent = mockContainerScrollParent($container);
@@ -35,9 +52,41 @@ describe("getScrollLeft", () => {
     });
 });
 
+describe("isScrolledIntoView", () => {
+    it("returns true when the element is visible", () => {
+        const $container = mock$JQueryElement({
+            height: 100,
+            offsetTop: 0,
+        });
+        const containerScrollParent = mockContainerScrollParent($container);
+
+        const $element = mock$JQueryElement({
+            height: 10,
+            offsetTop: 0,
+        });
+
+        expect(containerScrollParent.isScrolledIntoView($element)).toBe(true);
+    });
+
+    it("returns false when the element is not visible", () => {
+        const $container = mock$JQueryElement({
+            height: 100,
+            offsetTop: 0,
+        });
+        const containerScrollParent = mockContainerScrollParent($container);
+
+        const $element = mock$JQueryElement({
+            height: 10,
+            offsetTop: 150,
+        });
+
+        expect(containerScrollParent.isScrolledIntoView($element)).toBe(false);
+    });
+});
+
 describe("scrollToY", () => {
     it("sets scrollTop of the container", () => {
-        const $container = mock$Container({
+        const $container = mock$JQueryElement({
             scrollLeft: 10,
         });
         const containerScrollParent = mockContainerScrollParent($container);
