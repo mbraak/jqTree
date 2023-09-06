40d6f0d87e69458b9781c91b0404875d83e34c50
Marco Braak <marco@floryn.com>
2023-09-06 08:40:03 +0200

    Test wip

--- src/test/jqTree/scrollHandler/containerScrollParent.test.ts
+++ src/test/jqTree/scrollHandler/containerScrollParent.test.ts
@@ -1,11 +1,19 @@
 import ContainerScrollParent from "../../../scrollHandler/containerScrollParent";
 
-const mockContainerScrollParent = ({ scrollLeft } = { scrollLeft: 0 }) => {
+const mock$Container = ({ scrollLeft } = { scrollLeft: 0 }) => {
+    const container = {} as HTMLElement;
+
     const $container = {
+        get: (_i: number) => container,
         innerHeight: () => 0,
         offset: () => ({ top: 0 }),
         scrollLeft: () => scrollLeft,
     } as JQuery<HTMLElement>;
+
+    return $container;
+};
+
+const mockContainerScrollParent = ($container: JQuery<HTMLElement>) => {
     const refreshHitAreas = jest.fn();
     const $treeElement = {} as JQuery<HTMLElement>;
 
@@ -18,10 +26,23 @@ const mockContainerScrollParent = ({ scrollLeft } = { scrollLeft: 0 }) => {
 
 describe("getScrollLeft", () => {
     it("returns the scrollLeft of the container", () => {
-        const containerScrollParent = mockContainerScrollParent({
+        const $container = mock$Container({
             scrollLeft: 10,
         });
+        const containerScrollParent = mockContainerScrollParent($container);
 
         expect(containerScrollParent.getScrollLeft()).toBe(10);
+    });
+});
+
+describe("scrollToY", () => {
+    it("sets scrollTop of the container", () => {
+        const $container = mock$Container({
+            scrollLeft: 10,
+        });
+        const containerScrollParent = mockContainerScrollParent($container);
+        containerScrollParent.scrollToY(10);
+
+        expect($container.get(0)!.scrollTop).toBe(10);
     });
 });
