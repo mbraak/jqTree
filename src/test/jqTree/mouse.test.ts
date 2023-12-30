0abb25d0f02534d200ea858d76ff24ecdc83962f
Marco Braak <marco@floryn.com>
2023-12-30 12:58:22 +0100

    Add tests

--- src/test/jqTree/mouse.test.ts
+++ src/test/jqTree/mouse.test.ts
@@ -1,7 +1,7 @@
 import "../../tree.jquery";
 import { userEvent } from "@testing-library/user-event";
 import exampleData from "../support/exampleData";
-import { titleSpan } from "../support/testUtil";
+import { titleSpan, togglerLink } from "../support/testUtil";
 
 beforeEach(() => {
     $("body").append('<div id="tree1"></div>');
@@ -38,4 +38,45 @@ it("deselects when a selected node is clicked", async () => {
     await userEvent.click(titleSpan(node.element).get(0) as HTMLElement);
 
     expect(node.element).not.toBeSelected();
+});
+
+it("opens a node when the toggle button is clicked", async () => {
+    const $tree = $("#tree1");
+    $tree.tree({ data: exampleData });
+
+    const node = $tree.tree("getNodeByNameMustExist", "node1");
+    expect(node.element).not.toBeOpen();
+
+    await userEvent.click(togglerLink(node.element).get(0) as HTMLElement);
+
+    expect(node.element).toBeOpen();
+});
+
+it("doesn't select a node when it is opened", async () => {
+    const $tree = $("#tree1");
+    $tree.tree({ data: exampleData });
+
+    const node = $tree.tree("getNodeByNameMustExist", "node1");
+    expect(node.element).not.toBeSelected();
+    expect(node.element).not.toBeOpen();
+
+    await userEvent.click(togglerLink(node.element).get(0) as HTMLElement);
+
+    expect(node.element).not.toBeSelected();
+    expect(node.element).toBeOpen();
+});
+
+it("keeps it selected when a selected node is opened", async () => {
+    const $tree = $("#tree1");
+    $tree.tree({ data: exampleData });
+
+    const node = $tree.tree("getNodeByNameMustExist", "node1");
+    $tree.tree("selectNode", node);
+    expect(node.element).toBeSelected();
+    expect(node.element).not.toBeOpen();
+
+    await userEvent.click(togglerLink(node.element).get(0) as HTMLElement);
+
+    expect(node.element).toBeSelected();
+    expect(node.element).toBeOpen();
 });
