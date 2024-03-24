b719acc90ba6ee639eaca530504fa46b811465f2
Marco Braak <marco@floryn.com>
2024-03-24 09:30:08 +0100

    Refactor generateHitAreas: use HitPosition class

--- src/dragAndDropHandler/generateHitAreas.ts
+++ src/dragAndDropHandler/generateHitAreas.ts
@@ -4,28 +4,31 @@ import { Position } from "../position";
 import { getOffsetTop } from "../util";
 import iterateVisibleNodes from "./iterateVisibleNodes";
 
-const generatePositions = (tree: Node, currentNode: Node): HitArea[] => {
-    const positions: HitArea[] = [];
+interface HitPosition {
+    top: number;
+    node: Node;
+    position: Position;
+}
+
+const generateHitPositions = (tree: Node, currentNode: Node): HitPosition[] => {
+    const hitPositions: HitPosition[] = [];
     let lastTop = 0;
 
-    const addPosition = (node: Node, position: number, top: number) => {
-        const area = {
+    const addHitPosition = (node: Node, position: number, top: number) => {
+        hitPositions.push({
             top,
-            bottom: 0,
             node,
             position,
-        };
-
-        positions.push(area);
+        });
         lastTop = top;
     };
 
     const handleAfterOpenFolder = (node: Node, nextNode: Node | null) => {
         if (node === currentNode || nextNode === currentNode) {
             // Cannot move before or after current item
-            addPosition(node, Position.None, lastTop);
+            addHitPosition(node, Position.None, lastTop);
         } else {
-            addPosition(node, Position.After, lastTop);
+            addHitPosition(node, Position.After, lastTop);
         }
     };
 
@@ -38,20 +41,20 @@ const generatePositions = (tree: Node, currentNode: Node): HitArea[] => {
 
         if (node === currentNode) {
             // Cannot move after current item
-            addPosition(node, Position.None, top);
+            addHitPosition(node, Position.None, top);
         } else {
-            addPosition(node, Position.Inside, top);
+            addHitPosition(node, Position.Inside, top);
 
             // Cannot move before current item
             if (nextNode !== currentNode) {
-                addPosition(node, Position.After, top);
+                addHitPosition(node, Position.After, top);
             }
         }
     };
 
     const handleFirstNode = (node: Node) => {
         if (node !== currentNode) {
-            addPosition(node, Position.Before, getOffsetTop(node.element));
+            addHitPosition(node, Position.Before, getOffsetTop(node.element));
         }
     };
 
@@ -64,16 +67,16 @@ const generatePositions = (tree: Node, currentNode: Node): HitArea[] => {
 
         if (node === currentNode) {
             // Cannot move inside current item
-            addPosition(node, Position.None, top);
+            addHitPosition(node, Position.None, top);
         } else {
-            addPosition(node, Position.Inside, top);
+            addHitPosition(node, Position.Inside, top);
         }
 
         if (nextNode === currentNode || node === currentNode) {
             // Cannot move before or after current item
-            addPosition(node, Position.None, top);
+            addHitPosition(node, Position.None, top);
         } else {
-            addPosition(node, Position.After, top);
+            addHitPosition(node, Position.After, top);
         }
     };
 
@@ -84,11 +87,11 @@ const generatePositions = (tree: Node, currentNode: Node): HitArea[] => {
             // Dnd over the current element is not possible: add a position with type None for the top and the bottom.
             const top = getOffsetTop(element);
             const height = element.clientHeight;
-            addPosition(node, Position.None, top);
+            addHitPosition(node, Position.None, top);
 
             if (height > 5) {
                 // Subtract 5 pixels to allow more space for the next element.
-                addPosition(node, Position.None, top + height - 5);
+                addHitPosition(node, Position.None, top + height - 5);
             }
 
             // Stop iterating
@@ -97,7 +100,7 @@ const generatePositions = (tree: Node, currentNode: Node): HitArea[] => {
 
         // Cannot move before current item
         if (node.children[0] !== currentNode) {
-            addPosition(node, Position.Inside, getOffsetTop(element));
+            addHitPosition(node, Position.Inside, getOffsetTop(element));
         }
 
         // Continue iterating
@@ -112,12 +115,12 @@ const generatePositions = (tree: Node, currentNode: Node): HitArea[] => {
         handleOpenFolder,
     });
 
-    return positions;
+    return hitPositions;
 };
 
 const generateHitAreasForGroup = (
     hitAreas: HitArea[],
-    positionsInGroup: HitArea[],
+    positionsInGroup: HitPosition[],
     top: number,
     bottom: number,
 ) => {
@@ -127,11 +130,10 @@ const generateHitAreasForGroup = (
     const areaHeight = Math.round((bottom - top) / positionCount);
     let areaTop = top;
 
-    let i = 0;
-    while (i < positionCount) {
-        const position = positionsInGroup[i];
+    for (let i = 0; i < positionCount; i++) {
+        const position = positionsInGroup[i] as HitPosition;
 
-        if (position && position.position !== Position.None) {
+        if (position.position !== Position.None) {
             hitAreas.push({
                 top: areaTop,
                 bottom: areaTop + areaHeight,
@@ -141,19 +143,22 @@ const generateHitAreasForGroup = (
         }
 
         areaTop += areaHeight;
-        i += 1;
     }
 };
 
 const generateHitAreasFromPositions = (
-    positions: HitArea[],
+    hitPositions: HitPosition[],
     treeBottom: number,
 ): HitArea[] => {
-    let previousTop = positions[0]?.top ?? 0;
-    let group = [];
+    if (!hitPositions.length) {
+        return [];
+    }
+
+    let previousTop = (hitPositions[0] as HitArea).top;
+    let group: HitPosition[] = [];
     const hitAreas: HitArea[] = [];
 
-    for (const position of positions) {
+    for (const position of hitPositions) {
         if (position.top !== previousTop && group.length) {
             generateHitAreasForGroup(
                 hitAreas,
@@ -174,14 +179,10 @@ const generateHitAreasFromPositions = (
     return hitAreas;
 };
 
-const generateHitAreas = (
-    tree: Node,
-    currentNode: Node,
-    treeBottom: number,
-) => {
-    const positions = generatePositions(tree, currentNode);
-
-    return generateHitAreasFromPositions(positions, treeBottom);
-};
+const generateHitAreas = (tree: Node, currentNode: Node, treeBottom: number) =>
+    generateHitAreasFromPositions(
+        generateHitPositions(tree, currentNode),
+        treeBottom,
+    );
 
 export default generateHitAreas;
