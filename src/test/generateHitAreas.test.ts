385de0553de233e740cabc7c405c7ee0e47c0244
Marco Braak <marco@floryn.com>
2024-03-24 13:22:07 +0100

    Add test

--- src/test/generateHitAreas.test.ts
+++ src/test/generateHitAreas.test.ts
@@ -25,16 +25,14 @@ describe("generateHitAreasForGroup", () => {
         const hitAreas: HitArea[] = [];
         generateHitAreasForGroup(hitAreas, [hitPosition], 40, 100);
 
-        expect(hitAreas).toEqual(
-            expect.arrayContaining([
-                expect.objectContaining({
-                    bottom: 100,
-                    position: Position.Inside,
-                    node,
-                    top: 40,
-                }),
-            ]),
-        );
+        expect(hitAreas).toEqual([
+            expect.objectContaining({
+                bottom: 100,
+                position: Position.Inside,
+                node,
+                top: 40,
+            }),
+        ]);
     });
 
     it("doesn't add a hit area when the position is Position.None", () => {
@@ -68,22 +66,20 @@ describe("generateHitAreasForGroup", () => {
         const hitAreas: HitArea[] = [];
         generateHitAreasForGroup(hitAreas, hitPositions, 40, 100);
 
-        expect(hitAreas).toEqual(
-            expect.arrayContaining([
-                expect.objectContaining({
-                    bottom: 70,
-                    position: Position.Before,
-                    node,
-                    top: 40,
-                }),
-                expect.objectContaining({
-                    bottom: 100,
-                    position: Position.Inside,
-                    node,
-                    top: 70,
-                }),
-            ]),
-        );
+        expect(hitAreas).toEqual([
+            expect.objectContaining({
+                bottom: 70,
+                position: Position.Before,
+                node,
+                top: 40,
+            }),
+            expect.objectContaining({
+                bottom: 100,
+                position: Position.Inside,
+                node,
+                top: 70,
+            }),
+        ]);
     });
 });
 
@@ -100,16 +96,14 @@ describe("generateHitAreasFromPositions", () => {
             position: Position.Inside,
         };
 
-        expect(generateHitAreasFromPositions([hitPosition], 140)).toEqual(
-            expect.arrayContaining([
-                expect.objectContaining({
-                    bottom: 140,
-                    node,
-                    position: Position.Inside,
-                    top: 100,
-                }),
-            ]),
-        );
+        expect(generateHitAreasFromPositions([hitPosition], 140)).toEqual([
+            expect.objectContaining({
+                bottom: 140,
+                node,
+                position: Position.Inside,
+                top: 100,
+            }),
+        ]);
     });
 
     it("returns two hit areas with two hit positions with the same top", () => {
@@ -127,22 +121,20 @@ describe("generateHitAreasFromPositions", () => {
             },
         ];
 
-        expect(generateHitAreasFromPositions(hitPositions, 140)).toEqual(
-            expect.arrayContaining([
-                expect.objectContaining({
-                    bottom: 120,
-                    node,
-                    position: Position.Before,
-                    top: 100,
-                }),
-                expect.objectContaining({
-                    bottom: 140,
-                    node,
-                    position: Position.Inside,
-                    top: 120,
-                }),
-            ]),
-        );
+        expect(generateHitAreasFromPositions(hitPositions, 140)).toEqual([
+            expect.objectContaining({
+                bottom: 120,
+                node,
+                position: Position.Before,
+                top: 100,
+            }),
+            expect.objectContaining({
+                bottom: 140,
+                node,
+                position: Position.Inside,
+                top: 120,
+            }),
+        ]);
     });
 
     it("returns two hit areas with two hit positions with a different top", () => {
@@ -161,22 +153,20 @@ describe("generateHitAreasFromPositions", () => {
             },
         ];
 
-        expect(generateHitAreasFromPositions(hitPositions, 140)).toEqual(
-            expect.arrayContaining([
-                expect.objectContaining({
-                    bottom: 125,
-                    node: node1,
-                    position: Position.Inside,
-                    top: 100,
-                }),
-                expect.objectContaining({
-                    bottom: 140,
-                    node: node2,
-                    position: Position.After,
-                    top: 125,
-                }),
-            ]),
-        );
+        expect(generateHitAreasFromPositions(hitPositions, 140)).toEqual([
+            expect.objectContaining({
+                bottom: 125,
+                node: node1,
+                position: Position.Inside,
+                top: 100,
+            }),
+            expect.objectContaining({
+                bottom: 140,
+                node: node2,
+                position: Position.After,
+                top: 125,
+            }),
+        ]);
     });
 });
 
@@ -211,29 +201,96 @@ describe("generatePositions", () => {
             offsetParent: {},
         } as HTMLElement;
 
-        expect(generateHitPositions(tree, node1)).toEqual(
-            expect.arrayContaining([
-                expect.objectContaining({
-                    node: node1,
-                    position: Position.None,
-                    top: 100,
-                }),
-                expect.objectContaining({
-                    node: node1,
-                    position: Position.None,
-                    top: 100,
-                }),
-                expect.objectContaining({
-                    node: node2,
-                    position: Position.Inside,
-                    top: 120,
-                }),
-                expect.objectContaining({
-                    node: node2,
-                    position: Position.After,
-                    top: 120,
-                }),
-            ]),
-        );
+        expect(generateHitPositions(tree, node1)).toEqual([
+            expect.objectContaining({
+                node: node1,
+                position: Position.None,
+                top: 100,
+            }),
+            expect.objectContaining({
+                node: node1,
+                position: Position.None,
+                top: 100,
+            }),
+            expect.objectContaining({
+                node: node2,
+                position: Position.Inside,
+                top: 120,
+            }),
+            expect.objectContaining({
+                node: node2,
+                position: Position.After,
+                top: 120,
+            }),
+        ]);
+    });
+
+    it("generates hit positions when the tree has a node and an open folder node and the first node is the current node", () => {
+        const tree = new Node().loadFromData([
+            { name: "node1", id: 1 },
+            { name: "node2", id: 2, children: [{ name: "child1", id: 3 }] },
+        ]);
+
+        const node1 = tree.children[0] as Node;
+        const node2 = tree.children[1] as Node;
+        const child1 = node2.children[0] as Node;
+
+        node1.element = {
+            getBoundingClientRect: () => ({
+                x: 10,
+                y: 100,
+            }),
+            offsetParent: {},
+        } as HTMLElement;
+
+        node2.element = {
+            getBoundingClientRect: () => ({
+                x: 10,
+                y: 120,
+            }),
+            offsetParent: {},
+        } as HTMLElement;
+        node2.is_open = true;
+
+        child1.element = {
+            getBoundingClientRect: () => ({
+                x: 10,
+                y: 140,
+            }),
+            offsetParent: {},
+        } as HTMLElement;
+
+        expect(generateHitPositions(tree, node1)).toEqual([
+            expect.objectContaining({
+                node: node1,
+                position: Position.None,
+                top: 100,
+            }),
+            expect.objectContaining({
+                node: node1,
+                position: Position.None,
+                top: 100,
+            }),
+            expect.objectContaining({
+                node: node2,
+                position: Position.Inside,
+                top: 120,
+            }),
+            expect.objectContaining({
+                node: child1,
+                position: Position.Inside,
+                top: 140,
+            }),
+            expect.objectContaining({
+                node: child1,
+                position: Position.After,
+                top: 140,
+            }),
+            expect.objectContaining({
+                node: node2,
+                position: Position.After,
+                top: 140,
+            }),
+        ]);
     });
 });
