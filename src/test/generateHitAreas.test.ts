f8d44325cb5595a32ab0a9b4c9d4bf461b40ccb5
Marco Braak <marco@floryn.com>
2024-03-24 10:05:28 +0100

    Add test for one hit position

--- src/test/generateHitAreas.test.ts
+++ src/test/generateHitAreas.test.ts
@@ -1,10 +1,35 @@
 import { generateHitAreasForGroup } from "../dragAndDropHandler/generateHitAreas";
 import { HitArea } from "../dragAndDropHandler/types";
+import { Node } from "../node";
+import { Position } from "../position";
 
 describe("generateHitAreasForGroup", () => {
-    it("handles zero hit positions", () => {
+    it("doesn't add an hit area with zero hit positions", () => {
         const hitAreas: HitArea[] = [];
         generateHitAreasForGroup(hitAreas, [], 0, 0);
         expect(hitAreas).toBeEmpty();
+    });
+
+    it("adds one hitarea with one hit position", () => {
+        const node = new Node(null);
+        const hitPosition = {
+            top: 0,
+            node,
+            position: Position.Inside,
+        };
+
+        const hitAreas: HitArea[] = [];
+        generateHitAreasForGroup(hitAreas, [hitPosition], 0, 100);
+
+        expect(hitAreas).toEqual(
+            expect.arrayContaining([
+                expect.objectContaining({
+                    bottom: 100,
+                    position: Position.Inside,
+                    node,
+                    top: 0,
+                }),
+            ]),
+        );
     });
 });
