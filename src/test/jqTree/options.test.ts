6ab54eee89d665ebe4e8b39ed2f82781f7d75ea0
Marco Braak <marco@floryn.com>
2024-04-01 16:18:52 +0200

    Add test

--- src/test/jqTree/options.test.ts
+++ src/test/jqTree/options.test.ts
@@ -302,7 +302,7 @@ describe("dataUrl", () => {
         });
     });
 
-    it("loads the data and selects the node based on the state", async () => {
+    it("loads the data and selects the node when the state contains a selected node", async () => {
         localStorage.setItem("tree", '{"selected_node":[124]}');
 
         given.$tree.tree({
@@ -314,6 +314,18 @@ describe("dataUrl", () => {
         expect((given.$tree.tree("getSelectedNode") as INode).name).toBe(
             "node2",
         );
+    });
+
+    it("loads the data and doesn't selects a node when the state doesn't contain a selected node", async () => {
+        localStorage.setItem("tree", "{}");
+
+        given.$tree.tree({
+            dataUrl: { url: "/tree/" },
+            saveState: true,
+        });
+
+        await screen.findByText("node1");
+        expect(given.$tree.tree("getSelectedNode")).toBeFalse();
     });
 });
 
