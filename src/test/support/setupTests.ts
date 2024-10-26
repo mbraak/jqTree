25f637b151da1119b02c8528341ca8fdf5b42187
Marco Braak <marco@floryn.com>
2024-10-26 08:51:14 +0200

    Fix JQuery type in tests

--- src/test/support/setupTests.ts
+++ src/test/support/setupTests.ts
@@ -3,5 +3,12 @@ import jQuery from "jquery";
 
 import "./jqTreeMatchers";
 
-(window as any).$ = jQuery; // eslint-disable-line @typescript-eslint/no-unsafe-member-access
-(window as any).jQuery = jQuery; // eslint-disable-line @typescript-eslint/no-unsafe-member-access
+declare global {
+    interface Window {
+        $: JQueryStatic;
+        jQuery: JQueryStatic;
+    }
+}
+
+window.$ = jQuery;
+window.jQuery = jQuery;
