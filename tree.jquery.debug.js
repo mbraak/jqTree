d7efd9e98be85f82ee18f76c2e97fff70340312f
Marco Braak <marco@floryn.com>
2024-10-31 22:05:00 +0100

    Build

--- tree.jquery.debug.js
+++ tree.jquery.debug.js
@@ -1811,16 +1811,21 @@ var jqtree = (function (exports) {
         this.element = element;
         this.node = node;
         this.ghost = this.createGhostElement();
-        if (position === Position.After) {
-          this.moveAfter();
-        } else if (position === Position.Before) {
-          this.moveBefore();
-        } else if (position === Position.Inside) {
-          if (node.isFolder() && node.is_open) {
-            this.moveInsideOpenFolder();
-          } else {
-            this.moveInside();
-          }
+        switch (position) {
+          case Position.After:
+            this.moveAfter();
+            break;
+          case Position.Before:
+            this.moveBefore();
+            break;
+          case Position.Inside:
+            {
+              if (node.isFolder() && node.is_open) {
+                this.moveInsideOpenFolder();
+              } else {
+                this.moveInside();
+              }
+            }
         }
       }
       createGhostElement() {
