b0886c2bbbd150c5dc9f103143d26474c404ab0a
Marco Braak <marco@floryn.com>
2024-11-01 09:39:39 +0100

    Add test for ghostDropHint

--- src/test/nodeElement/ghostDropHint.test.ts
+++ src/test/nodeElement/ghostDropHint.test.ts
@@ -2,6 +2,10 @@ import { Node } from "../../node";
 import GhostDropHint from "../../nodeElement/ghostDropHint";
 import { Position } from "../../position";
 
+beforeEach(() => {
+    document.body.innerHTML = "";
+});
+
 it("creates a hint element after the node element when the position is After", () => {
     const nodeElement = document.createElement("div");
     document.body.append(nodeElement);
@@ -10,4 +14,52 @@ it("creates a hint element after the node element when the position is After", (
     new GhostDropHint(node, nodeElement, Position.After);
 
     expect(nodeElement.nextSibling).toHaveClass("jqtree-ghost");
+    expect(nodeElement.previousSibling).toBeNull();
+    expect(nodeElement.children).toBeEmpty();
+});
+
+it("creates a hint element after the node element when the position is Before", () => {
+    const nodeElement = document.createElement("div");
+    document.body.append(nodeElement);
+
+    const node = new Node();
+    new GhostDropHint(node, nodeElement, Position.Before);
+
+    expect(nodeElement.previousSibling).toHaveClass("jqtree-ghost");
+    expect(nodeElement.nextSibling).toBeNull();
+    expect(nodeElement.children).toBeEmpty();
+});
+
+it("creates a hint element after the node element when the position is Inside and the node is an open folder", () => {
+    const nodeElement = document.createElement("div");
+    document.body.append(nodeElement);
+
+    const childElement = document.createElement("div");
+    nodeElement.append(childElement);
+
+    const node = new Node({ is_open: true });
+    const childNode = new Node();
+    childNode.element = childElement;
+    node.addChild(childNode);
+
+    new GhostDropHint(node, nodeElement, Position.Inside);
+
+    expect(nodeElement.previousSibling).toBeNull();
+    expect(nodeElement.nextSibling).toBeNull();
+    expect(nodeElement.children.length).toBe(2);
+    expect(nodeElement.children[0]).toHaveClass("jqtree-ghost");
+});
+
+it("creates a hint element after the node element when the position is Inside and the node is a closed folder", () => {
+    const nodeElement = document.createElement("div");
+    document.body.append(nodeElement);
+
+    const node = new Node();
+    node.addChild(new Node());
+
+    new GhostDropHint(node, nodeElement, Position.Inside);
+
+    expect(nodeElement.nextSibling).toHaveClass("jqtree-ghost");
+    expect(nodeElement.previousSibling).toBeNull();
+    expect(nodeElement.children).toBeEmpty();
 });
