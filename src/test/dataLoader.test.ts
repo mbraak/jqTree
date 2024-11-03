6fdd0c90c05b7ee539a83780c43a2951ef932ba8
Marco Braak <marco@floryn.com>
2024-11-03 15:46:43 +0100

    Test dataloader

--- src/test/dataLoader.test.ts
+++ src/test/dataLoader.test.ts
@@ -1,4 +1,22 @@
+import { waitFor } from "@testing-library/dom";
+import { http, HttpResponse } from "msw";
+import { setupServer } from "msw/node";
+
 import DataLoader from "../dataLoader";
+
+const server = setupServer();
+
+afterAll(() => {
+    server.close();
+});
+
+afterEach(() => {
+    server.resetHandlers();
+});
+
+beforeAll(() => {
+    server.listen();
+});
 
 describe("loadFromUrl", () => {
     it("does nothing when urlInfo is empty", () => {
@@ -14,5 +32,35 @@ describe("loadFromUrl", () => {
 
         dataLoader.loadFromUrl(null, null, null);
         expect(triggerEvent).not.toHaveBeenCalled();
+    });
+
+    it("parses json when the response is a string", async () => {
+        server.use(
+            http.get(
+                "/test",
+                () =>
+                    new HttpResponse('{ "key1": "value1" }', {
+                        headers: {
+                            "Content-Type": "text/plain",
+                        },
+                    }),
+                {},
+            ),
+        );
+
+        const loadData = jest.fn();
+        const treeElement = document.createElement("div");
+        const triggerEvent = jest.fn();
+
+        const dataLoader = new DataLoader({
+            loadData,
+            treeElement,
+            triggerEvent,
+        });
+        dataLoader.loadFromUrl({ dataType: "text", url: "/test" }, null, null);
+
+        await waitFor(() => {
+            expect(loadData).toHaveBeenCalledWith({ key1: "value1" }, null);
+        });
     });
 });
