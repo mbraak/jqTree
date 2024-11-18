83b1b8ce89728ca32ad9230d337a1c316f039577
Marco Braak <marco@floryn.com>
2024-11-18 16:15:02 +0100

    Test setInitialState

--- src/test/saveStateHandler.test.ts
+++ src/test/saveStateHandler.test.ts
@@ -1,13 +1,15 @@
+import { Node } from "../node";
 import SaveStateHandler from "../saveStateHandler";
 
-const createSaveStateHandler = () => {
+const createSaveStateHandler = ({
+    getSelectedNodes = jest.fn(),
+    removeFromSelection = jest.fn(),
+}) => {
     const addToSelection = jest.fn();
     const getNodeById = jest.fn();
-    const getSelectedNodes = jest.fn();
     const getTree = jest.fn();
     const openNode = jest.fn();
     const refreshElements = jest.fn();
-    const removeFromSelection = jest.fn();
 
     return new SaveStateHandler({
         addToSelection,
@@ -29,16 +31,33 @@ describe("getStateFromStorage", () => {
     it("returns null when the state is not in local storage", () => {
         localStorage.clear();
 
-        const saveStateHandler = createSaveStateHandler();
+        const saveStateHandler = createSaveStateHandler({});
         expect(saveStateHandler.getStateFromStorage()).toBeNull();
     });
 
     it("returns an array of selected nodes when 'selected_node' in the states is a number", () => {
         localStorage.setItem("tree", JSON.stringify({ selected_node: 123 }));
 
-        const saveStateHandler = createSaveStateHandler();
+        const saveStateHandler = createSaveStateHandler({});
         expect(saveStateHandler.getStateFromStorage()).toEqual({
             selected_node: [123],
         });
+    });
+});
+
+describe("setInitialState", () => {
+    it("deselects nodes that are currently selected", () => {
+        const node = new Node({ id: 123 });
+
+        const getSelectedNodes = jest.fn(() => [node]);
+        const removeFromSelection = jest.fn();
+
+        const saveStateHandler = createSaveStateHandler({
+            getSelectedNodes,
+            removeFromSelection,
+        });
+        saveStateHandler.setInitialState({});
+
+        expect(removeFromSelection).toHaveBeenCalledWith(node);
     });
 });
