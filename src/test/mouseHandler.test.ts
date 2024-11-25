736cc4de67ca95bc8fffc69ac95e57116e877f09
Marco Braak <marco@floryn.com>
2024-11-25 10:48:29 +0100

    Add tests for handleContextmenu

--- src/test/mouseHandler.test.ts
+++ src/test/mouseHandler.test.ts
@@ -5,12 +5,14 @@ interface CreateMouseHandlerParams {
     element: HTMLElement;
     getNode?: jest.Mock;
     onClickButton?: jest.Mock;
+    triggerEvent?: jest.Mock;
 }
 
 const createMouseHandler = ({
     element,
     getNode = jest.fn(),
     onClickButton = jest.fn(),
+    triggerEvent = jest.fn(),
 }: CreateMouseHandlerParams) => {
     const getMouseDelay = jest.fn();
     const onClickTitle = jest.fn();
@@ -18,7 +20,6 @@ const createMouseHandler = ({
     const onMouseDrag = jest.fn();
     const onMouseStart = jest.fn();
     const onMouseStop = jest.fn();
-    const triggerEvent = jest.fn();
 
     return new MouseHandler({
         element,
@@ -31,7 +32,7 @@ const createMouseHandler = ({
         onMouseStart,
         onMouseStop,
         triggerEvent,
-        useContextMenu: false,
+        useContextMenu: true,
     });
 };
 
@@ -65,14 +66,81 @@ describe("handleClick", () => {
         expect(onClickButton).toHaveBeenCalledWith(node);
     });
 
-    // eslint-disable-next-line jest/expect-expect
     it("handles a click with an empty target", () => {
         const element = document.createElement("div");
-        createMouseHandler({ element });
+        const onClickButton = jest.fn();
+        createMouseHandler({ element, onClickButton });
 
         const event = new MouseEvent("click");
         jest.spyOn(event, "target", "get").mockReturnValue(null);
 
         element.dispatchEvent(event);
+
+        expect(onClickButton).not.toHaveBeenCalled();
+    });
+});
+
+describe("handleContextmenu", () => {
+    it("handles a context menu event on a node", () => {
+        const treeElement = document.createElement("ul");
+        treeElement.classList.add("jqtree-tree");
+        document.body.appendChild(treeElement);
+
+        const nodeElement = document.createElement("div");
+        nodeElement.className = "jqtree-element";
+        treeElement.appendChild(nodeElement);
+
+        const node = new Node();
+
+        const getNode = jest.fn((element: HTMLElement) => {
+            if (element === nodeElement) {
+                return node;
+            } else {
+                return null;
+            }
+        });
+
+        const triggerEvent = jest.fn();
+
+        createMouseHandler({ element: nodeElement, getNode, triggerEvent });
+
+        const event = new MouseEvent("contextmenu", { bubbles: true });
+        nodeElement.dispatchEvent(event);
+
+        expect(triggerEvent).toHaveBeenCalledWith("tree.contextmenu", {
+            click_event: event,
+            node,
+        });
+    });
+
+    it("handles a context menu event that's not on a node", () => {
+        const element = document.createElement("div");
+        document.body.appendChild(element);
+
+        const getNode = jest.fn(() => null);
+        const triggerEvent = jest.fn();
+
+        createMouseHandler({ element, getNode, triggerEvent });
+
+        const event = new MouseEvent("contextmenu", { bubbles: true });
+        element.dispatchEvent(event);
+
+        expect(triggerEvent).not.toHaveBeenCalled();
+    });
+
+    it("handles a context menu event without a target", () => {
+        const element = document.createElement("div");
+        document.body.appendChild(element);
+
+        const triggerEvent = jest.fn();
+
+        createMouseHandler({ element, triggerEvent });
+
+        const event = new MouseEvent("contextmenu", { bubbles: true });
+        jest.spyOn(event, "target", "get").mockReturnValue(null);
+
+        element.dispatchEvent(event);
+
+        expect(triggerEvent).not.toHaveBeenCalled();
     });
 });
