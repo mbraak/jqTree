a1be570d7189d7499cfe4cd91a0364572416c4ed
Marco Braak <marco@floryn.com>
2024-12-28 14:17:06 +0100

    Use hero icons

--- docs/static/examples/icon_buttons.js
+++ docs/static/examples/icon_buttons.js
@@ -1,12 +1,20 @@
 $.mockjax({
     url: "*",
-    response: function(options) {
+    response: function () {
         this.responseText = ExampleData.exampleData;
     },
-    responseTime: 0
+    responseTime: 0,
 });
 
 $("#tree1").tree({
-    closedIcon: $('<i class="fas fa-arrow-circle-right"></i>'),
-    openedIcon: $('<i class="fas fa-arrow-circle-down"></i>')
+    closedIcon: $(
+        `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
+        </svg>`,
+    ),
+    openedIcon: $(
+        `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 13.5 12 21m0 0-7.5-7.5M12 21V3" />
+        </svg>`,
+    ),
 });
