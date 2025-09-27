3f1a4350c86cd2149a8080cd6b3996c03ae00418
Marco Braak <marco@floryn.com>
2025-09-27 11:26:46 +0200

    Coverage config

--- config/vitest.config.ts
+++ config/vitest.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     coverage: {
+      exclude: ['src/playwright'],
+      include: ['src/**'],
       provider: "istanbul",
       reporter: ["lcov", "text"],
     },
