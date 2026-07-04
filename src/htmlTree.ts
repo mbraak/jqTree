846e1ee74c61861fcd30b388753062b0dd091d62
Marco Braak <marco@floryn.com>
2026-07-04 14:20:40 +0200

    setDefaultOptions

--- src/htmlTree.ts
+++ src/htmlTree.ts
@@ -1,44 +1,18 @@
 import type { JQTreeOptions } from "./jqtreeOptions";
 
+import setDefaultOptions from "./htmlTree/setDefaultOptions";
+
 export default class HtmlTree {
+  public htmlElement: HTMLElement;
   public isInitialized: boolean;
   public options: JQTreeOptions;
-
-  private htmlElement: HTMLElement;
 
   constructor(htmlElement: HTMLElement, options: JQTreeOptions) {
     this.htmlElement = htmlElement;
     this.options = options;
 
+    setDefaultOptions(htmlElement, options);
+
     this.isInitialized = false;
-
-    this.options.rtl ??= this.getRtlOption();
-    this.options.closedIcon ??= this.getDefaultClosedIcon();
-  }
-
-  private getDefaultClosedIcon(): string {
-    if (this.options.rtl) {
-      // triangle to the left
-      return "&#x25c0;";
-    } else {
-      // triangle to the right
-      return "&#x25ba;";
-    }
-  }
-
-  private getRtlOption(): boolean {
-    if (this.options.rtl != null) {
-      return this.options.rtl;
-    } else {
-      const dataRtl = this.htmlElement.dataset.rtl;
-
-      if (
-        dataRtl !== undefined && dataRtl !== 'false'
-      ) {
-        return true;
-      } else {
-        return false;
-      }
-    }
   }
 }
---
+++ src/htmlTree/setDefaultOptions.ts
@@ -0,0 +1,34 @@
+import type { JQTreeOptions } from "../jqtreeOptions";
+
+const setDefaultOptions = (htmlElement: HTMLElement, options: JQTreeOptions) => {
+  options.rtl ??= getRtlOption(htmlElement, options);
+  options.closedIcon ??= getDefaultClosedIcon(options);
+}
+
+const getDefaultClosedIcon = (options: JQTreeOptions): string => {
+  if (options.rtl) {
+    // triangle to the left
+    return "&#x25c0;";
+  } else {
+    // triangle to the right
+    return "&#x25ba;";
+  }
+}
+
+const getRtlOption = (htmlElement: HTMLElement, options: JQTreeOptions): boolean => {
+  if (options.rtl != null) {
+    return options.rtl;
+  } else {
+    const dataRtl = htmlElement.dataset.rtl;
+
+    if (
+      dataRtl !== undefined && dataRtl !== 'false'
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+export default setDefaultOptions;
