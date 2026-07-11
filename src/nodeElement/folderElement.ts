bcef416bccc54a08ba8674889b66197ed3f39db7
Marco Braak <marco@floryn.com>
2026-07-11 07:42:47 +0200

    Replace jquery hide and show (#967)
    * Set display style to hide folder
    
    * Replace hide and show
    
    * Build

--- src/nodeElement/folderElement.ts
+++ src/nodeElement/folderElement.ts
@@ -68,7 +68,8 @@ class FolderElement extends NodeElement {
         if (slide) {
             jQuery(this.getUl()).slideUp(animationSpeed, doClose);
         } else {
-            jQuery(this.getUl()).hide();
+            const ul = this.getUl();
+            ul.style.display = "none";
             doClose();
         }
     }
@@ -113,7 +114,8 @@ class FolderElement extends NodeElement {
         if (slide) {
             jQuery(this.getUl()).slideDown(animationSpeed, doOpen);
         } else {
-            jQuery(this.getUl()).show();
+            const ul = this.getUl();
+            ul.style.display = "block";
             doOpen();
         }
     }
