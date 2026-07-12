99f543735224f095956ce3ae808a045b454e86f9
Marco Braak <marco@floryn.com>
2026-07-12 13:21:43 +0200

    Make examples work with fetch (#970)
    * Use msw for one example
    
    * Implement for all examples
    
    * Clean up

--- docs/static/examples/load_on_demand.js
+++ docs/static/examples/load_on_demand.js
@@ -1,15 +1,23 @@
-$.ajax = function (settings) {
-    setTimeout(function () {
-        if (settings.data && settings.data.node) {
-            settings.success(ExampleData.getChildrenOfNode(settings.data.node));
+const handlers = [
+    MockServiceWorker.http.get("/nodes/", ({ request }) => {
+        const url = new URL(request.url);
+        const parentId = url.searchParams.get("node");
+
+        if (parentId) {
+            return MockServiceWorker.HttpResponse.json(
+                ExampleData.getChildrenOfNode(parentId),
+            );
         } else {
-            settings.success(ExampleData.getFirstLevelData());
+            return MockServiceWorker.HttpResponse.json(
+                ExampleData.getFirstLevelData(),
+            );
         }
-    }, 1000);
-};
+    }),
+];
 
-var $tree = $("#tree1");
-
-$tree.tree({
-    saveState: true,
+mockServer(handlers).then(() => {
+    const $tree = $("#tree1");
+    $tree.tree({
+        saveState: true,
+    });
 });
