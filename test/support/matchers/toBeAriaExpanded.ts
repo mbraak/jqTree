5810d8fdb35ec050493ce4ea62a433d3e0516800
Marco Braak <marco@floryn.com>
2026-07-25 08:48:16 +0200

    Fix istanbul ignore

--- test/support/matchers/toBeAriaExpanded.ts
+++ test/support/matchers/toBeAriaExpanded.ts
@@ -1,9 +1,9 @@
 import type { Matcher, MatcherState } from 'vitest'
 
 export const toBeAriaExpanded: Matcher = function (this: MatcherState, el: HTMLElement) {
-  /* istanbul ignore next @preserve */
   const { isNot } = this
 
+  /* istanbul ignore next @preserve */
   return {
     message: () => `The element is${isNot ? "" : "not "} ARIA expanded`,
     pass: el.ariaExpanded === "true",
--- test/support/matchers/toBeAriaSelected.ts
+++ test/support/matchers/toBeAriaSelected.ts
@@ -1,9 +1,9 @@
 import type { Matcher, MatcherState } from 'vitest'
 
 export const toBeAriaSelected: Matcher = function (this: MatcherState, el: HTMLElement) {
-  /* istanbul ignore next @preserve */
   const { isNot } = this
 
+  /* istanbul ignore next @preserve */
   return {
     message: () => `The element is${isNot ? "" : "not "} ARIA selected`,
     pass: el.ariaSelected === "true",
